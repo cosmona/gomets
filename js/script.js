dc179bd30af1f27754a3baf768b60319a700cf71
xusCrus <jesus.m.cruz.h@gmail.com>
2014-01-25 03:06:43 +0100

    can change player

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).on('ready', inicio);
 
 function inicio ()
 {
-    $('.face-container').on('click', add_gomet);
+    //$('.face-container').on('click', add_gomet);
 }
 
 function add_gomet (e)
@@ -30,14 +30,14 @@ function PlayerController ($scope) {
             name: 'Ramon',
             gomets: {
                 red: [{x: 41, y: 143}],
-                green: []
+                green: [{x: 88, y: 50}, {x: 12, y: 400}]
             }
         },
         {
             name: 'Jesus',
             gomets: {
-                red: [],
-                green: []
+                red: [{x: 41, y: 143}],
+                green: [{x: 41, y: 143}]
             }
         }
     ];
@@ -45,10 +45,29 @@ function PlayerController ($scope) {
     $scope.current = $scope.players[0];
 
 
-    function selectPlayer (p) {
-        alert('mierda');
+    $scope.selectPlayer = function (p) {
+        console.log(p);
         $scope.current = p;
     }
 
 
+    $scope.addGomet = function (e)
+    {
+        alert('mierda');
+        $.ionSound({
+            sounds: ["punch", "green"],
+            path: "media/sound/",
+            volume: "0.3"
+        });
+
+        $.ionSound.play("punch");
+        //$.ionSound.play("green");
+        return;
+
+        var offset = $(this).offset();
+        var fix = 15;
+        var x = e.clientX - offset.left - fix;
+        var y = e.clientY - offset.top - fix;
+        $(this).append('<div class="gomet" style="left:' + x + 'px;top:' + y + 'px;"></div>');
+    }
 }
