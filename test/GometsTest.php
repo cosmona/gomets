424368c428622edd1f8a8958548cb4e8d33ca655
xusCrus <jesus.m.cruz.h@gmail.com>
2014-01-26 19:52:41 +0100

    setting up phpunit

---
+++ test/GometsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GometsTest
+ *
+ * @author jesus
+ */
+class GometsTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    function cantTest()
+    {
+        $this->assertTrue(false);
+    }
+
+}
