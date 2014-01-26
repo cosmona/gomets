f946db593be35b6c4457633f242f92bf313f138f
xusCrus <jesus.m.cruz.h@gmail.com>
2014-01-26 18:46:40 +0100

    moving to public

---
+++ public/js/script.js
@@ -0,0 +1,52 @@
+function PlayerController ($scope) {
+    $scope.players = [
+        {
+            name: 'Ramon',
+            gomets: {
+                red: [],
+                green: []
+            }
+        },
+        {
+            name: 'Jesus',
+            gomets: {
+                red: [],
+                green: []
+            }
+        }
+    ];
+
+    $scope.current = $scope.players[0];
+
+    $scope.gometType = 'red';
+
+
+    $scope.selectPlayer = function (player) {
+        $scope.current = player;
+    };
+
+    $scope.isActive = function (player) {
+        if (player == $scope.current) {
+            return 'active';
+        }
+    };
+
+
+    $scope.addGomet = function (event)
+    {
+        $.ionSound({
+            sounds: ["red", "green"],
+            path: "media/sound/",
+            volume: "0.3"
+        });
+
+        $.ionSound.play($scope.gometType);
+
+        var offset = $('.face-container').offset();
+        var fix = 15;
+        var x = event.clientX - offset.left - fix;
+        var y = event.clientY - offset.top - fix;
+
+        $scope.current.gomets[$scope.gometType].push({x: x, y: y});
+    };
+}
