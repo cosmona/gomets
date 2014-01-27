a51cfeedafe270511ad88cc14685dc8fbc1f3323
xusCrus <jesus.m.cruz.h@gmail.com>
2014-01-27 03:14:45 +0100

    cleaning

--- conf/setUp.php
+++ conf/setUp.php
@@ -1,30 +1,3 @@
 <?php
 
 define('APP_FOLDER', dirname(__DIR__));
-
-if (!defined('PHPUNIT_TESTSUITE')) {
-    $confjson = file_get_contents(__DIR__ . '/production.json');
-    $dev = false;
-} else {
-    $confjson = file_get_contents(__DIR__ . '/developement.json');
-    $dev = true;
-}
-
-$conf = json_decode($confjson);
-
-$database = APP_FOLDER . '/db/' . $conf->database;
-/*
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-
-$isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(APP_FOLDER . "/src"), $isDevMode);
-
-$conn = array(
-    'driver' => 'pdo_sqlite',
-    'path' => $database,
-);
-
-// obtaining the entity manager
-$entityManager = EntityManager::create($conn, $config);
-*/
